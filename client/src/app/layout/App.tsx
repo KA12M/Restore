ae79f1ce1a59539ef310ec0e528f4b2706d30ec9
KARM <pj@example.com>
2022-08-27 16:45:19 +0700

    latest

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
+import Catalog from "../../features/catalog/Catalog";
+import { Container } from "@mui/system";
 
 const App = () => {
   const [themeMode, setThemeMode] = useState(false);
@@ -12,11 +14,15 @@ const App = () => {
   });
 
   const handleTheme = () => setThemeMode(!themeMode);
-  
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header handleMode={handleTheme} themeMode={themeMode} />
+
+      <Container>
+        <Catalog />
+      </Container>
     </ThemeProvider>
   );
 };
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from "@mui/material";
 
 const Catalog = () => {
-  return (
-    <div>Catalog</div>
-  )
-}
+  const data = Array.from(Array(16).keys());
 
-export default Catalog
+  const CardProduct = () => (
+    <Card
+    // sx={{ maxWidth: "100%" }}
+    >
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="240"
+        image={`http://placeimg.com/640/480/animals?${Math.random()}`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Elwyn
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Share</Button>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
+
+  return (
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      // columns={{ xs: 3, sm: 4, md: 6, lg: 12 }}
+    >
+      {data.map((_, index) => (
+        <Grid item lg={3} md={4} sm={6} xs={12} key={index}>
+          <CardProduct />
+        </Grid>
+      ))}
+    </Grid>
+  );
+};
+
+export default Catalog;
