abeab84aa365046c45d2befb849319c64712f2b7
KARM <pj@example.com>
2022-08-27 15:31:29 +0700

    latest

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "./Header";
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
-
 const App = () => {
+  const [themeMode, setThemeMode] = useState(false);
+
+  const theme = createTheme({
+    palette: {
+      mode: themeMode ? "dark" : "light",
+    },
+  });
+
+  const handleTheme = () => setThemeMode(!themeMode);
+  
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Header />
     </ThemeProvider>
